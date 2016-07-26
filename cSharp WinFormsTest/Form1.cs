f076906d9c75b8ce27eecce3ebce5856b5d5d9a6
Ryan Woodcock <ryanguy426@gmail.com>
2016-07-26 15:33:57 -0400

    Extra comment

--- cSharp WinFormsTest/Form1.cs
+++ cSharp WinFormsTest/Form1.cs
@@ -12,14 +12,14 @@ namespace cSharp_WinFormsTest
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1() // Startup of program/form. Do not overload this. Make any heavy code in here multithreaded.
         {
-            InitializeComponent();
+            InitializeComponent(); // Required to start the program, I believe. Anything under this is called on program startup.
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) // Event for clicking the specified control in the form (in this case, the button).
         {
-            MessageBox.Show("Hello, World!", "Hi, there!");
+            MessageBox.Show("Hello, World!", "Hi, there!"); // User clicked the button, so say "Hello"!
         }
     }
 }
